04eddcb41c413386d8a22be15a88a1c3b6880663
Aidan Clyens <aidanc@rogers.com>
2019-12-15 17:58:00 -0500

    Init pygame and run function

--- snake_game/game.py
+++ snake_game/game.py
@@ -1,9 +1,32 @@
+import pygame
+
 class Game:
     def __init__(self):
-        pass
+        pygame.init()
+
+        self._clock = pygame.Clock()
+
+        self._running = True
 
     def run(self):
-        pass
+        while self.running:
+            self._poll_events()
+
+            self.update()
+            self.render()
+
+            self._clock.tick(60)
 
     def quit(self):
+        pygame.quit()
+    
+    def poll_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._running = False
+    
+    def update(self):
+        pass
+
+    def render(self):
         pass
