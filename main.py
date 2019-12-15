3d2fd760ccbd3a0c0bcc59da1777b8f4b0e33b9c
Aidan Clyens <aidanc@rogers.com>
2019-12-15 17:53:04 -0500

    Added basic game structure

---
+++ main.py
@@ -0,0 +1,12 @@
+from snake_game import Game
+
+
+def main():
+    Game game
+
+    game.run()
+
+    game.quit()
+
+if __name__ == '__main__':
+    main()
---
+++ snake_game/game.py
@@ -0,0 +1,9 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def quit(self):
+        pass
