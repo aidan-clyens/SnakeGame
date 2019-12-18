2f44d9479d4101e6767bac91e97fa244325edcc4
Aidan Clyens <aidanc@rogers.com>
2019-12-17 20:24:42 -0500

    Update block rect position

--- snake_game/block.py
+++ snake_game/block.py
@@ -8,10 +8,17 @@ class Block:
 
         self._image = pygame.Surface(self._size)
         self._image.fill(pygame.Color(255, 255, 255))
+        self._rect = self._image.get_rect()
 
     def update(self, pos=None):
         if pos is not None:
             self._position = pos
 
+        self._rect.x = self._position[0]
+        self._rect.y = self._position[1]
+
     def render(self, screen):
-        screen.blit(self._image, self._position)
+        screen.blit(self._image, [self._rect.x, self._rect.y])
+
+    def collides(self, block):
+        return self._rect.colliderect(block._rect)
